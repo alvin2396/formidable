c5d0b97b89d53476105cbbc4f65f931c617e12ba
AdlinMH <adlin.asus@gmail.com>
2017-06-30 17:48:34 +0700

    tambah penjelasan coding

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ app.post('/', function(req, res){
     console.log('Uploaded ' + file.name);
   })
 
-    res.render('index')
+  // render views/index.pug
+  res.render('index')
 })
 
 /*==================================================
@@ -56,24 +57,28 @@ app.post('/', function(req, res){
  * menggunakan method GET untuk menuju halaman list -> list.html
  */
 app.get('/list', function (req, res){
+  // deklarasi variabel array kosong
   var arrImgName = [];
+
+  // mulai baca file di folder gambar
   fs.readdir(__dirname + '/' + img_folder, function(err, filenames)
   {
+    // lakukan looping untuk tiap file yang dibaca di folder yang dimaksud
     filenames.forEach(function(item, index) {
+      // melakukan filter ektensi file hanya untuk gambar
       var ext = item.split(".")[1];
-      console.log(ext)
       if(ext== "jpg" || ext== "png" || ext== "gif") {
-        console.log("ll-" + item)
+        // masukkan nama file ke variabel gambar
         arrImgName.push(item)
       }
     });
-    console.log(arrImgName)
+    // kirim variabel dan render views/list.pug
     res.render('list', { arrImg: arrImgName})
   })
 
 })
 
-/*
- * Running Simple Application on PORT 8123
+/*==================================================
+ * Running Application on PORT 8123
  */
 app.listen(8123);
